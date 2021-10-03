8a1e0758817c8f0508b11aea858e7a1e6c52753e
Sagar <sagarpatil1917118@gmail.com>
2021-10-03 23:26:08 +0530

    Adding product actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,22 @@
-import {createStore,combineReducers,applyMiddleware} from 'react-redux';
+import {createStore,combineReducers,applyMiddleware} from 'redux';
 
 import thunk from "redux-thunk"
 
-import {composeWithDevtool} from "redux-devtool-extension"
+import {composeWithDevTools} from "redux-devtools-extension"
+import { productReducer } from './reducers/productReducer';
+
+const reducer = combineReducers({
+     products: productReducer,
+});
+
+let initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;
