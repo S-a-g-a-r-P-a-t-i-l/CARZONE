b3146e5835fa33b58e6525304bb7713bb50fc54d
harshal <harshalrane114@gmail.com>
2021-10-04 13:38:00 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
-const { clearScreenDown } = require("readline");
+const app = require("./app");
 
-clearScreenDown
+const dotenv = require("dotenv");
+const connectDatabase =require("./config/database");
+
+//config
+dotenv.config({path:"backend/config/config.env"});
+
+// Connecting to database
+connectDatabase();
+
+
+app.listen(process.env.PORT,()=>{
+
+   console.log(`Server is working on http://localhost:${process.env.PORT}`)
+})
