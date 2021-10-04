0b264a1b75abb2b4e1d0a806feacaeaf6e42700e
HarshalRaneHR <87926328+HarshalRaneHR@users.noreply.github.com>
2021-10-04 15:07:38 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express =require("express");
 const app =express();
 
@@ -7,15 +7,10 @@ app.use(express.json())
 //Route Imports
 const product =require("./routes/productRoute");
 
-app.use("/api/v1",product);
-=======
-const express = require("express");
-const app = express();
 
-app.use(express.json())
-//Routes imports
-const product = require("./routes/productRoute");
-app.use("/api/v1",product)
->>>>>>> 23d0edf2063d28596ecbf8e93876ad9c0d4988d3
+
+
+app.use("/api/v1",product);
+
 
 module.exports = app
