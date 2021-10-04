fbea0981087c092f52363967af68fd1bdd9f899b
HarshalRaneHR <87926328+HarshalRaneHR@users.noreply.github.com>
2021-10-04 15:09:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-<<<<<<< HEAD
+
 const dotenv = require("dotenv");
 const connectDatabase =require("./config/database");
 
@@ -14,16 +14,7 @@ connectDatabase();
 app.listen(process.env.PORT,()=>{
 
    console.log(`Server is working on http://localhost:${process.env.PORT}`)
-})
-=======
-const dotemv= require("dotenv");
 
 
-//config
-
-dotemv.config({path:"backend/config/config.env"})
-app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
   });
-  
->>>>>>> 23d0edf2063d28596ecbf8e93876ad9c0d4988d3
+
