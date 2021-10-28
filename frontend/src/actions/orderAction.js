89be1d8f2f182e8a2f4f6097157d9421c287b130
Akash <akashrokade1432@gmail.com>
2021-10-29 00:13:24 +0530

    Dashboard created

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -1,24 +1,24 @@
 import {
-    CREATE_ORDER_REQUEST,
-    CREATE_ORDER_SUCCESS,
-    CREATE_ORDER_FAIL,
-    MY_ORDERS_REQUEST,
-    MY_ORDERS_SUCCESS,
-    MY_ORDERS_FAIL,
-    ALL_ORDERS_REQUEST,
-    ALL_ORDERS_SUCCESS,
-    ALL_ORDERS_FAIL,
-    UPDATE_ORDER_REQUEST,
-    UPDATE_ORDER_SUCCESS,
-    UPDATE_ORDER_FAIL,
-    DELETE_ORDER_REQUEST,
-    DELETE_ORDER_SUCCESS,
-    DELETE_ORDER_FAIL,
-    ORDER_DETAILS_REQUEST,
-    ORDER_DETAILS_SUCCESS,
-    ORDER_DETAILS_FAIL,
-    CLEAR_ERRORS,
-  } from "../constants/orderConstants.js";
+  CREATE_ORDER_REQUEST,
+  CREATE_ORDER_SUCCESS,
+  CREATE_ORDER_FAIL,
+  MY_ORDERS_REQUEST,
+  MY_ORDERS_SUCCESS,
+  MY_ORDERS_FAIL,
+  ALL_ORDERS_REQUEST,
+  ALL_ORDERS_SUCCESS,
+  ALL_ORDERS_FAIL,
+  UPDATE_ORDER_REQUEST,
+  UPDATE_ORDER_SUCCESS,
+  UPDATE_ORDER_FAIL,
+  DELETE_ORDER_REQUEST,
+  DELETE_ORDER_SUCCESS,
+  DELETE_ORDER_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  CLEAR_ERRORS,
+} from "../constants/orderConstants";
   
   import axios from "axios";
   
@@ -100,21 +100,21 @@ import {
     }
   };
   
-  // Delete Order
-  export const deleteOrder = (id) => async (dispatch) => {
-    try {
-      dispatch({ type: DELETE_ORDER_REQUEST });
-  
-      const { data } = await axios.delete(`/api/v1/admin/order/${id}`);
-  
-      dispatch({ type: DELETE_ORDER_SUCCESS, payload: data.success });
-    } catch (error) {
-      dispatch({
-        type: DELETE_ORDER_FAIL,
-        payload: error.response.data.message,
-      });
-    }
-  };
+// Delete Order
+export const deleteOrder = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: DELETE_ORDER_REQUEST });
+
+    const { data } = await axios.delete(`/api/v1/admin/order/${id}`);
+
+    dispatch({ type: DELETE_ORDER_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: DELETE_ORDER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
   
   // Get Order Details
   export const getOrderDetails = (id) => async (dispatch) => {
